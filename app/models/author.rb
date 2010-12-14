e9744b4591ef6f6d93ccb1ddb3795561aadb6af1
faust45 <faust451@gmail.com>
2010-12-14 16:52:35 +0500

    Add author AvadhutMj

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,7 +4,7 @@ class Author < BaseModel
   TEACHERS = %w(AcharyaMj GovindaMj SridharMj SarasvatiThakur BabajiGaurakishor BhaktivinodThakur DjaganathBabaji)
   ACHARYA_LIB = %w(GovindaMj SridharMj SarasvatiThakur BhaktivinodThakur)
   AUTHORS_LIB = %w(BharatiMj GoswamiMj)
-  MATH_AUTHORS = %w(BharatiMj GoswamiMj SrutasravaPr SiddhantiMj)
+  MATH_AUTHORS = %w(BharatiMj GoswamiMj SrutasravaPr SiddhantiMj AvadhutMj)
 
   NAMES_IDS = {
     'Б.Н. Ачарья Махарадж'  => 'AcharyaMj',
