f27b66eb7f9883bfe11668266ba220649980f3f0
faust45 <faust451@gmail.com>
2011-01-02 11:52:29 +0500

    HTML Update Teachers

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,15 +1,7 @@
 module TeachersHelper
-  def name(teacher)
-    "<span>#{teacher.display_name}</span>".html_safe
+
+  def teacher_photo(teacher)
+    photo_thumb(teacher.main_photo_page_menu, {:width => "117", :height => "183"}, true)
   end
 
-  def teacher_li(teacher)
-    photo = photo_thumb(teacher.main_photo_page_menu, {:width => "117", :height => "183"}, true) + name(teacher)
-
-    if @teacher == teacher
-      content_tag(:strong, photo)
-    else
-      link_to photo, teacher_show_path(teacher.id)
-    end
-  end
 end
