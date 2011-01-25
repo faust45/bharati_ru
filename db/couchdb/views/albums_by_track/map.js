9fc11ad8ce86454e96dd5e82b1df76a6f8c74fac
faust45 <faust451@gmail.com>
2011-01-25 16:59:05 +0200

    Fix view albums_by_track

--- db/couchdb/views/albums_by_track/map.js
+++ db/couchdb/views/albums_by_track/map.js
@@ -1,7 +1,7 @@
 function(doc) {
-  if(doc['couchrest-type'] == 'Album') {
+  if(doc['couchrest-type'] == 'Album' || doc['couchrest-type'] == 'SbAlbum') {
     if(doc.tracks) {
-       for(i in doc.tracks) {
+       for(var i in doc.tracks) {
          emit(doc.tracks[i], null);
        };
      }
