dcc28396d0dfa6eec50c64446c7537d5c86a9f02
faust45 <faust451@gmail.com>
2011-02-19 21:42:08 +0200

    Delete deprecations

--- lib/audio_form_adapter.rb
+++
@@ -1,9 +0,0 @@
-class AudioFormAdapter < ContentFormAdapter
-
-  class <<self
-    def model
-      Audio
-    end
-  end
-
-end
--- lib/video_form_adapter.rb
+++
@@ -1,9 +0,0 @@
-class VideoFormAdapter < ContentFormAdapter
-
-  class <<self
-    def model
-      Video 
-    end
-  end
-
-end
