b9e2cf9603102cdf291b24fb1fac71adcbe3407b
faust45 <faust451@gmail.com>
2011-12-08 22:38:59 +0200

    fix path

--- lib/site_path.rb
+++ lib/site_path.rb
@@ -20,7 +20,7 @@ class SitePath
       if params[:controller] == "main" && params[:action] == 'index'
         link = link_to((helper.image_tag('logo.png')).html_safe, root_path)
       else
-        link = link_to((helper.image_tag('logo-all.png')).html_safe, root_path)
+        link = link_to('<span>Бхарати<span>.ру</span></span>'.html_safe, root_path)
       end
       Path.new(link)
     end
